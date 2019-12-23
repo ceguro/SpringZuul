854f657354294d83535b7700af7e1dcb948ee68d
ceguro <carlos.gutierrez@globant.com>
2019-12-23 16:52:28 -0300

    refactor on log methods

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -15,33 +15,21 @@ public class TestController {
     @PostMapping(path = "/answer1")
     public String test1(HttpServletRequest request) {
 
-        log.info("Request Received /api :");
-        log.info("Verb: {}", request.getMethod());
-        log.info("URL: {}", request.getRequestURL());
-        log.info("Host: {}", request.getRemoteHost());
-
+        logs(request);
         return "Test success 1";
     }
 
     @GetMapping(path = "/answer2")
     public String test2(HttpServletRequest request) {
 
-        log.info("Request Received /api :");
-        log.info("Verb: {}", request.getMethod());
-        log.info("URL: {}", request.getRequestURL());
-        log.info("Host: {}", request.getRemoteHost());
-
+        logs(request);
         return "Test success 2";
     }
 
     @PutMapping(path = "/{id}")
     public String test3(HttpServletRequest request) {
 
-        log.info("Request Received /api :");
-        log.info("Verb: {}", request.getMethod());
-        log.info("URL: {}", request.getRequestURL());
-        log.info("Host: {}", request.getRemoteHost());
-
+        logs(request);
         return "Test success 3";
     }
 
@@ -50,11 +38,7 @@ public class TestController {
             @PathVariable long id, HttpServletRequest request) {
 
 
-        log.info("Request Received /api :");
-        log.info("Verb: {}", request.getMethod());
-        log.info("URL: {}", request.getRequestURL());
-        log.info("Host: {}", request.getRemoteHost());
-
+        logs(request);
         /*
         log.info("Request Received /api :");
         log.info("Verb: {}", request.getMethod());
@@ -86,17 +70,17 @@ public class TestController {
     }
 
     @PostMapping("/{id}/answer6/{example}")
-    public ResponseEntity  findById3(
+    public ResponseEntity findById3(
             @PathVariable long id,
             @PathVariable String example,
             HttpServletRequest request) {
-        return ResponseEntity.ok("{\"empresa\":{\"codigo\":\"ERHV\",\"orden\":76,\"tipoEmpresa\":\"G\",\"nombre\":\"GCBA No Tributarios\"," +
+        return ResponseEntity.ok("{\"empresa\":{\"codigo\":\"ERHV\",\"orden\":76,\"tipoEmpresa\":\"G\",\"nombre\":\"XXXX No Tributarios\"," +
                 "\"tituloIdentificacion\":\"Cod.pagos por redes(ptes)\",\"permiteUsd\":false,\"tipoPago\":3,\"soloConsultas\":false," +
                 "\"permiteDobleFacturacion\":true,\"importePermitido\":0,\"codigoMoneda\":1,\"datoAdicional\":\"\",\"tarjetasPermitidas\":[\"13\",\"43\"]," +
                 "\"montoMinimoTC\":0,\"montoMaximoTC\":0,\"rubro\":{\"idRubro\":\"MCPS\",\"tipo\":\"0\",\"nombre\":\"Impuestos Municipales\"}," +
                 "\"permitePagosTCV\":false,\"permitePagosTCM\":false,\"permitePagosTCA\":false,\"permitePagosTDV\":true,\"permitePagosTDM\":true," +
                 "\"rubroID\":\"MCPS\"},\"fechaPago\":\"2019-11-13T18:20:34.000+0000\",\"transacNum\":\"9413\",\"controlNum\":\"4313\"," +
-                "\"idDeuda\":\"00100026137501\",\"descDeuda\":\"GCBA NO TRIBU\",\"leyPago\":\"COMPROBANTE VALIDO DE PAGO.CONSERVELO.\"," +
+                "\"idDeuda\":\"00100026137501\",\"descDeuda\":\"XXXX NO TRIBU\",\"leyPago\":\"COMPROBANTE VALIDO DE PAGO.CONSERVELO.\"," +
                 "\"leyAdhesion\":\"Pagos GCBA INT RegCiv\",\"monto\":315,\"moneda\":32,\"cuenta\":{\"numero\":\"0\",\"tipo\":\"0\"," +
                 "\"moneda\":\"0\",\"cbu\":\"\",\"estado\":\"0\",\"saldo\":0,\"limite\":0,\"intereses\":0,\"ctaEspecial\":false}," +
                 "\"tarjeta\":{\"nroTarjeta\":\"Visa D\u00e9bito XXXX-1408\",\"codigoMedioDePago\":\"03\",\"cuotas\":\"01\",\"tipoTarjeta\":\"TD\"," +
@@ -104,5 +88,15 @@ public class TestController {
 
     }
 
+    private void logs(HttpServletRequest request) {
+
+        log.info("Request Received /api :");
+        log.info("Verb: {}", request.getMethod());
+        log.info("URL: {}", request.getRequestURL());
+        log.info("Host: {}", request.getRemoteHost());
+
+    }
+
 
 }
+
